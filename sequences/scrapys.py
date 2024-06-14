789e161a89d1511628270ec3e08bb56895c81d95
Rodolfo Lima <rodolfo.eli.jrles@gmail.com>
2024-06-13 21:43:28 -0300

    get_reads

--- sequences/scrapys.py
+++ sequences/scrapys.py
@@ -1,9 +1,18 @@
 import json
-from Bio import Entrez
+from Bio import Entrez, SeqIO
 
-def getseq(db: str, id: str, rettype: str, retmode:str ):
-    Entrez.email = "joserodolfo.silva@ufpe.br"
+
+user = Entrez.email = "joserodolfo.silva@ufpe.br"
+# Comando para baixar sequências de nucleotídeos
+def getsequences(db: str, id: str, rettype: str, retmode:str ):
     handle = Entrez.efetch(db=db,id=id,rettype=rettype,retmode=retmode)
     with open("NZ_CP041838.1.fasta","w") as arquivo:
         arquivo.write(handle.read())
     handle.close()
+    
+# Comanando para baixar informações do arquivo SRA  
+def get_reads(ids, db='nucleotide'):
+    handle = Entrez.efetch(db=db, id=ids, rettyoe='fasta', retmode='text')
+    sequences = list(SeqIO.parse(handle, "fasta"))
+    handle.close()
+    return sequences
